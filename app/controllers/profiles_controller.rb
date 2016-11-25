e359d571c6d9e9f21a66f8846edddb76c7c8d716
jefferson oliveira <cardbeatt@gmail.com>
2016-11-25 17:58:56 -0200

    profile crud created

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,10 +1,36 @@
 class ProfilesController < ApplicationController
   def edit
+    @profile = Profile.find(params[:id])
+  end
+
+  def new
+    @profile = Profile.new
+  end
+
+  def create
+    @profile = Profile.new(profile_params)
+    if @profile.save
+      flash[:success] = "Perfil criado com sucesso!"
+      redirect_to root_path
+    else
+      render 'new'
+    end
   end
 
   def update
+    @profile = Profile.find(params[:id])
+    if @profile.update_attributes(profile_params)
+      flash[:success] = 'Perfil atualizado com sucesso !'
+    else
+      flash[:error] = 'Erro na atualização do perfil !'
+      render 'edit'
+    end
   end
 
-  def index
+  def show
+  end
+
+  def profile_params
+    params.require(:profile).permit(:username, :lastname, :subnick)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,5 @@
 Rails.application.routes.draw do
-  get 'profiles/edit'
-
-  get 'profiles/update'
-
-  get 'profiles/index'
-
+  resources :profiles
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
